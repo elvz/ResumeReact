5a54a973acb1bebca2efadf75a94bec2d3091e36
elvz <djelvz@gmail.com>
2018-11-08 16:50:43 +0200

    Updates

---
+++ precache-manifest.8d02a45966d1d5cbaffc8f447c1b3ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5574f4b68f586dad1be6",
+    "url": "/resumereact/static/js/runtime~main.5574f4b6.js"
+  },
+  {
+    "revision": "8cb383a75d6f753c03a3",
+    "url": "/resumereact/static/js/main.8cb383a7.chunk.js"
+  },
+  {
+    "revision": "02744d60281830af5b61",
+    "url": "/resumereact/static/js/1.02744d60.chunk.js"
+  },
+  {
+    "revision": "8cb383a75d6f753c03a3",
+    "url": "/resumereact/static/css/main.510b1286.chunk.css"
+  },
+  {
+    "revision": "02744d60281830af5b61",
+    "url": "/resumereact/static/css/1.45a3e6f7.chunk.css"
+  },
+  {
+    "revision": "8bcf6dafac8fdabeff56ed225ac92ce5",
+    "url": "/resumereact/index.html"
+  }
+];
